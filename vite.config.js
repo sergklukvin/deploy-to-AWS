700ba3ae7c943c7199fce9928c9beedeb44b3f78
Serhii Kliuch <skliu@softserveinc.com>
2024-01-27 17:48:07 +0200

    add test config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,8 +10,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    css: true,
     setupFiles: './src/test/setup.js',
-    reporters: ['json'],
-    outputFile: 'test.json'
+    // reporters: ['json'],
+    // outputFile: 'test.json'
   },
 });
